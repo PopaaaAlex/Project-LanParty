a756115221db13b9a3edc6f25ea93ec1b0f6bf69
Popa Alexandru <alex_macho_1234@DESKTOP-I0292P0>
2024-04-02 18:14:31 +0300

    inceputul

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 FILE *fisier1,*fisier2;
 
 int main()
 {
-    //fisier1=fopen("d.in",rt);
-    //fisier2=fopen("r.out",wt);
+    fisier1=fopen("date/t1/d.in","rt");
+    fisier2=fopen("r.out","w");
     
+    if(fisier1 == NULL)
+        {
+            printf("nu s-a putut deschide");
+            exit(1);
+        }
+    char date[256];
+    while(feof(fisier1) == 0)
+    {
+        fgets( date, sizeof(date), fisier1);
+    }
+
+    fclose(fisier1);
+    
+    if(fisier2 == NULL)
+        {
+            printf("nu s-a putut deschide");
+            exit(1);
+        }
+    fclose(fisier2);
 
 }
